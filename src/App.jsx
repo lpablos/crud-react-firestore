ef839297113eb667b7c4df38da3c6d6929645fb1
Luis Jorge P.S <lpablo@toshiba.com>
2021-05-03 22:29:46 -0500

    leer documentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from 'react'
+import { db } from './firebase'
+
 
 function App() {
+  const [tareas, setTareas] = useState([])
+
+  useEffect(() => {
+    obtenerDatos()    
+  }, [])
+
+  const obtenerDatos = async () =>{
+    
+    const data = await db.collection('tareas').get()    
+    const arrayData = data.docs.map(doc=>({id: doc.id, ...doc.data()}))
+    setTareas(arrayData)
+    console.log("Estas son las tareas ", tareas);
+   
+  }
   return (
-    <div class="container">
-      <h2>Hola Mundo</h2>
+    <div class="container mr-3">
+      <div class="row">
+        <div class="col-md-6">
+          <h2>Lista de tareas</h2>
+          <ul class="list-group">
+            {
+              tareas.map(item=>(
+                <li class="list-group-item" key={item.id}>{item.name}</li>
+              ))
+            }
+          </ul>
+        </div>
+        <div class="col-md-6">
+          formulario
+        </div>
+      </div>
+      
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import app from 'firebase/app'
-import 'firebase/firestore'
- 
+import 'firebase/firebase-firestore'
+
 var firebaseConfig = {
     apiKey: "AIzaSyCuiS7GTeQ0FGsOu_6T1jBfi79GUA49pJc",
     authDomain: "crud-react-firebase-312614.firebaseapp.com",
@@ -10,6 +10,8 @@ var firebaseConfig = {
     appId: "1:148356775824:web:4868505a8ebfc9482e7d15"
 };
 // Initialize Firebase
-app.initializeApp(firebaseConfig);
+app.initializeApp(firebaseConfig)
 
-export {app}
+const db = app.firestore()
+
+export { db }
