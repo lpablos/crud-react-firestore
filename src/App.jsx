44bfd506414fd7f1bdcd30984c5fcfba69ed9567
Luis Jorge P.S <lpablo@toshiba.com>
2021-05-05 05:02:14 -0500

    add taks and render list of taks

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,40 @@ import { db } from './firebase'
 
 function App() {
   const [tareas, setTareas] = useState([])
+  const [tarea, setTarea] = useState('')
 
   useEffect(() => {
     obtenerDatos()    
   }, [])
 
-  const obtenerDatos = async () =>{
-    
+  const obtenerDatos = async () =>{    
     const data = await db.collection('tareas').get()    
     const arrayData = data.docs.map(doc=>({id: doc.id, ...doc.data()}))
-    setTareas(arrayData)
-    console.log("Estas son las tareas ", tareas);
-   
+    setTareas(arrayData)   
   }
+
+  const agregar = async (e) =>{
+    e.preventDefault();
+    if(!tarea.trim()){
+      console.log('Esta vacio');
+      return
+    }
+    try{
+      let newtask = {
+        name : tarea,
+        date : Date.now()
+      }
+      let data = await db.collection('tareas').add(newtask)
+      setTarea('')
+      setTareas([
+        ...tareas,
+        {...newtask, id: data.id}
+      ])
+    }catch(error){
+      console.log(error);
+    }
+  }
+
   return (
     <div class="container mr-3">
       <div class="row">
@@ -31,7 +52,20 @@ function App() {
           </ul>
         </div>
         <div class="col-md-6">
-          formulario
+          <h2>Formulario</h2>
+          <form onSubmit={agregar} method="post">
+            <div class="mb-3">
+              <input type="text" 
+                placeholder="Ingrese tarea" 
+                class="form-control" 
+                onChange={e => setTarea(e.target.value)}
+                value={tarea}/>
+              
+              <div class="d-grid gap-2 mt-2">
+                <button type="submit" class="btn btn-primary btn-sm">Agregar</button>                
+              </div>
+            </div>
+          </form>
         </div>
       </div>
       
